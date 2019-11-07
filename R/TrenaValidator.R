e0adab417d8184f2f5a9d190eb4b347e64906a8f
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-11-07 15:00:48 -0800

    buildBindingSitesTable now ready

--- R/TrenaValidator.R
+++ R/TrenaValidator.R
@@ -3,7 +3,11 @@
 #' @importFrom AnnotationDbi select
 #' @import org.Hs.eg.db
 #' @import TrenaProject
-#' @import TrenaProjectErythropoiesis
+#' @import TrenaProjectHG38.generic
+#' @import MotifDb
+#' @import phastCons7way.UCSC.hg38
+#' @import GenomicScores
+
 #'
 #' @title TrenaValidator-class
 #'
@@ -25,8 +29,10 @@
                                ))
 
 #----------------------------------------------------------------------------------------------------
+setGeneric('fallbackEnhancers',  signature='obj', function(obj) standardGeneric('fallbackEnhancers'))
 setGeneric('findEnhancers',  signature='obj', function(obj, eliteOnly) standardGeneric('findEnhancers'))
-setGeneric('getEnhancerTable',   signature='obj', function(obj) standardGeneric('getEnhancerTable'))
+setGeneric('getTFBS',  signature='obj', function(obj, tbl.regions, fimo.threshold, conservation.threshold, pwmFile)
+              standardGeneric('getTFBS'))
 #----------------------------------------------------------------------------------------------------
 #' Define an object of class TrenaValidator
 #'
@@ -41,22 +47,40 @@ setGeneric('getEnhancerTable',   signature='obj', function(obj) standardGeneric(
 #'
 
 TrenaValidator <- function(TF, targetGene, mtx, tbl.benchmark, quiet=TRUE)
-
 {
+   source("~/github/fimoService/batchMode/fimoBatchTools.R")   # works on hagfish & khaleesi
+
    state <- new.env(parent=emptyenv())
-   tp <- TrenaProjectErythropoiesis()
+   tp <- TrenaProjectHG38.generic()
    setTargetGene(tp, targetGene)
-   tbl.geneInfo <- getTranscriptsTable(tp)
-   tbl.fallbackEnhancers <- with(tbl.geneInfo,
-                                 data.frame(chrom=chrom, start=tss-5000, end=tss+5000,
-                                            stringsAsFactors=FALSE))
-   state$enhancers <- tbl.fallbackEnhancers
 
    .TrenaValidator(TF=TF, targetGene=targetGene, matrix=mtx,
                    tbl.benchmark=tbl.benchmark, state=state, trenaProject=tp, quiet=quiet)
 
 } # TrenaValidator, the constructor
 #----------------------------------------------------------------------------------------------------
+#' a simple single 10k regulatory region used if no geneHancers values are avaialable
+#'
+#' @param obj An instance of the TrenaValidator class
+#'
+#' @return data.frame
+#'
+#' @export
+#'
+#' @aliases fallbackEnhancers
+#' @rdname fallbackEnhancers
+
+setMethod('fallbackEnhancers',  'TrenaValidator',
+
+   function(obj) {
+     tbl.geneInfo <- getTranscriptsTable(obj@tp)
+     tbl.fallbackEnhancers <- with(tbl.geneInfo,
+                                   data.frame(chrom=chrom, start=tss-5000, end=tss+5000,
+                                              stringsAsFactors=FALSE))
+     tbl.fallbackEnhancers
+     })
+
+#---------------------------------------------------------------------------------------------------
 #' query and store the GeneHancer regions for the targetGene
 #'
 #' @param obj An instance of the TrenaValidator class
@@ -75,11 +99,13 @@ setMethod('findEnhancers',  'TrenaValidator',
          tbl.enhancers <- getEnhancers(obj@trenaProject)
          if(eliteOnly)
             tbl.enhancers <- subset(tbl.enhancers, elite)
-         obj@state$enhancers <- tbl.enhancers
+         if(nrow(tbl.enhancers) == 0)
+            return(fallbackEnhancers(ob))
+         return(tbl.enhancers)
          })
 
 #----------------------------------------------------------------------------------------------------
-#' return the enhancers
+#' return the TFBS
 #'
 #' @param obj An instance of the TrenaValidator class
 #'
@@ -90,10 +116,19 @@ setMethod('findEnhancers',  'TrenaValidator',
 #' @aliases getEnhancerTable
 #' @rdname getEnhancerTable
 
-setMethod('getEnhancerTable',  'TrenaValidator',
+setMethod('getTFBS',  'TrenaValidator',
 
-      function(obj){
-         obj@state$enhancers
+      function(obj, tbl.regions, fimo.threshold, conservation.threshold, pwmFile){
+         tbl.match <- fimoBatch(tbl.regions, matchThreshold=fimo.threshold, genomeName="hg38",
+                                pwmFile=pwmFile)
+         if(conservation.threshold > 0){
+            tbl.match <- as.data.frame(gscores(phastCons7way.UCSC.hg38,
+                                               GRanges(tbl.match)), stringsAsFactors=FALSE)
+            tbl.match <- subset(tbl.match, default >= conservation.threshold)
+            colnames(tbl.match)[1] <- "chrom"
+            colnames(tbl.match)[grep("default", colnames(tbl.match))] <- "phast7"
+            }
+         tbl.match
          })
 
 #----------------------------------------------------------------------------------------------------
--- inst/unitTests/test_TrenaValidator.R
+++ inst/unitTests/test_TrenaValidator.R
@@ -1,18 +1,23 @@
 library(TrenaValidator)
 library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
-if(!exists("tProj")) {
+if(!exists("tv")) {
    message(sprintf("--- creating instance of TrenaValidator"))
    tbl.benchmark <- get(load(system.file(package="TrenaValidator", "extdata", "tbl.A.RData")))
    mtx <- get(load(system.file(package="TrenaValidator", "extdata", "mtx.gtex.lung.RData")))
    tv <- TrenaValidator(TF="TWIST1", "MMP2", mtx, tbl.benchmark);
    }
 #------------------------------------------------------------------------------------------------------------------------
+motifs <- query(MotifDb, c("hsapie"), c("jaspar2018", "hocomoco"))
+export(motifs, con="jaspar2018-hocomoc-human.meme", format="meme")
+#----------------------------------------------------------------------------------------------------
+
 runTests <- function()
 {
    test_constructor()
    test_enhancers()
-   
+   test_buildBindingSitesTable()
+
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
 test_constructor <- function()
@@ -26,15 +31,35 @@ test_constructor <- function()
 test_enhancers <- function()
 {
    message(sprintf("--- test_enhancers"))
-   tbl.fallback.enhancers <- getEnhancerTable(tv)
-   checkEquals(with(getEnhancerTable(tv), end-start), 10000)
-
-   findEnhancers(tv, eliteOnly=TRUE)
-   tbl.enhancers <- getEnhancerTable(tv)
-   checkTrue(nrow(tbl.enhancers) > 9)
-   checkTrue(sum(with(tbl.enhancers, end-start)) > 25000)
+   tbl.gh <- findEnhancers(tv, eliteOnly=TRUE)
+   checkTrue(nrow(tbl.gh) > 5)
 
 } # test_constructor
+#------------------------------------------------------------------------------------------------------------------------
+test_buildBindingSitesTable <- function()
+{
+   message(sprintf(printf("--- test_buildBindingSitesTable")))
+   motifs <- query(MotifDb, "hsapiens", c("jaspar2018", "hocomoco"))
+   meme.file <- "human.hsapiens.hocomoco.meme"
+   export(motifs, con=meme.file, format="meme")
+
+   tbl.gh <- findEnhancers(tv, eliteOnly=TRUE)
+   widths <- with(tbl.gh, end-start)
+   keepers <- which(widths < 500)
+
+   tbl.tfbs.1 <- getTFBS(tv, tbl.gh[1,], fimo.threshold=1e-3, conservation.threshold=0.95, meme.file)
+   checkTrue(nrow(tbl.tfbs.1) < 5)
+
+   tbl.tfbs.2 <- getTFBS(tv, tbl.gh[1,], fimo.threshold=1e-3, conservation.threshold=0, meme.file)
+   checkTrue(nrow(tbl.tfbs.2) > 1000)
+
+   tbl.tfbs.3 <- getTFBS(tv, tbl.gh[1,], fimo.threshold=1e-2, conservation.threshold=0.95, meme.file)
+   checkTrue(nrow(tbl.tfbs.3) > 100)
+
+   tbl.tfbs.4 <- getTFBS(tv, tbl.gh[1,], fimo.threshold=1e-3, conservation.threshold=0.75, meme.file)
+   checkTrue(nrow(tbl.tfbs.4) > 5)
+
+} # test_buildBindingSitesTable
 #------------------------------------------------------------------------------------------------------------------------
 if(!interactive())
    runTests()
